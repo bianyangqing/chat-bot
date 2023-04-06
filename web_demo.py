dd570db54d08afcdda4bac48593981c39561534e
黄茵 <huangyin@huangyindeMacBook-Pro.local>
2023-04-06 16:10:57 +0800

    add model name

--- web_demo.py
+++ web_demo.py
@@ -29,6 +29,7 @@ def predict(input, max_length, top_p, temperature, model_name, history=None):
 
 
 with gr.Blocks() as demo:
+    logging.warning("web_demo_start")
     state = gr.State([])
     text_boxes = []
     for i in range(MAX_BOXES):
@@ -45,7 +46,7 @@ with gr.Blocks() as demo:
             max_length = gr.Slider(0, 4096, value=2048, step=1.0, label="Maximum length", interactive=True)
             top_p = gr.Slider(0, 1, value=0.7, step=0.01, label="Top P", interactive=True)
             temperature = gr.Slider(0, 1, value=0.95, step=0.01, label="Temperature", interactive=True)
-            model_name = gr.inputs.Radio(["ChatGLM-6B", "chatGpt-api", "aliXX"], label="Model")
+            model_name = gr.inputs.Radio(["ChatGLM-6B", "chatGpt-api", "aliXX"], label="Model", interactive=True)
             button = gr.Button("Generate")
     button.click(predict, [txt, max_length, top_p, temperature, model_name, state], [state] + text_boxes)
 demo.queue().launch(share=True, inbrowser=True)
