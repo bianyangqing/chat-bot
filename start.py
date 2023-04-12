352a4b0abd18c5bba3e14afd7336858ce4844bc9
黄茵 <huangyin@huangyindeMacBook-Pro.local>
2023-04-12 23:36:04 +0800

    fix histroy

--- start.py
+++ start.py
@@ -46,9 +46,10 @@ def stream_chat(question, history=None, box_size=20):
         delta_content = " " if delta_content is None else delta_content
         content = content + delta_content
         logging.warning("content:{}".format(content))
-        updates.append(gr.update(visible=True, value="User：" + question))
-        updates.append(gr.update(visible=True, value="ChatGLM-6B：" + content))
         history.append((question, content))
+        for question, content in history:
+            updates.append(gr.update(visible=True, value="User：" + question))
+            updates.append(gr.update(visible=True, value="ChatGLM-6B：" + content))
         if len(updates) < box_size:
             updates = updates + [gr.Textbox.update(visible=False)] * (box_size - len(updates))
         yield [history] + updates
